4b3e139620d558ac33f66db8562c79ff7ed736e3
Ahmet Mircik <ahmetmircik@gmail.com>
2024-12-31 16:05:54 +0300

    wip repo url

--- hazelcast/project.clj
+++ hazelcast/project.clj
@@ -5,8 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [jepsen "0.3.4"]
-                 [com.hazelcast/hazelcast-enterprise "6.0.0-SNAPSHOT"]]
-  :repositories {"hazelcast snapshot" "https://repository.hazelcast.com/snapshot/"
-                 "hazelcast release" "https://repository.hazelcast.com/release/"}
+                 [com.hazelcast/hazelcast-enterprise "6.0.0-SNAPSHOT"
+                  :git/url "https://github.com/ahmetmircik/hazelcast-mono.git"
+                  :git/sha "6.0/cp/chunked"]]
+  :repositories {"hazelcast release" "https://repository.hazelcast.com/release/"}
   :aot [jepsen.hazelcast]
   :main jepsen.hazelcast)
